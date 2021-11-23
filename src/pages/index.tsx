354b23c43352e30b60d24a95adac92f608bbe514
Matias Ignacio Brabermoin <matias.brabermoin@globant.com>
2021-11-23 09:21:58 -0300

    a

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import Card from './Card.js'
-export default function IndexPage(): JSX.Element {
-  return <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+export default class index extends React.Component {
+  render(){
+    return(<div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
     <div style={{ height: "50%", width: "50%" }}>
       <Card></Card>
     </div>
-  </div>;
+  </div>)
+  }
+  
 }
